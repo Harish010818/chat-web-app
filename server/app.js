d779ae405c01a321db8bb7f85dfe9c49ac0cacc9
Harish Juyal <devharishjuyal18@gmail.com>
2025-05-19 13:44:19 +0530

    cors bug fix

--- server/app.js
+++ server/app.js
@@ -6,12 +6,14 @@ import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./socket/socket.js";
-dotenv.config({});
+import bodyParser from "body-parser";
+dotenv.config();
+connectDB();
 
 
 // middleware
-app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true}));
 
@@ -24,7 +26,6 @@ app.use("/api/v1/message", messageRoute);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-    connectDB();
     console.log(`Server listen at prot ${PORT}`);
 });
 
