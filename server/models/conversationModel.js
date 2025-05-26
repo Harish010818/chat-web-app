4ba640269fcc8dc9fe8f7625fb1f2d9c77a27545
Harish Juyal <devharishjuyal18@gmail.com>
2025-05-27 02:24:20 +0530

    added time stamp to model

--- server/models/conversationModel.js
+++ server/models/conversationModel.js
@@ -4,6 +4,6 @@ const conversationModel = new mongoose.Schema({
       Participants : [ {type : mongoose.Schema.Types.ObjectId, ref : "User"} ],
       messages : [ {type : mongoose.Schema.Types.ObjectId, ref : "Message"} ],
       lastMessage : { type: mongoose.Schema.Types.ObjectId, ref: "Message" } 
-})
+},{timestamps: true})
 
 export const Conversation = mongoose.model('Conversation', conversationModel);
