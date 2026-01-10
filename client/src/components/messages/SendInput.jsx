00ecf84411089fcfa3439b2d54ddb5e62ef9b874
Harish Juyal <devharishjuyal18@gmail.com>
2026-01-10 23:58:26 +0530

    feat: recording UI update

--- client/src/components/messages/SendInput.jsx
+++ client/src/components/messages/SendInput.jsx
@@ -1,71 +1,98 @@
 import { IoSend } from "react-icons/io5";
-import { Plus, SmilePlus } from 'lucide-react';
+import { Plus, SmilePlus } from "lucide-react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { setMessages } from '../../useRedux/messageSlice';
-import toast from 'react-hot-toast';
+import { setMessages } from "../../useRedux/messageSlice";
+import toast from "react-hot-toast";
+import { Mic } from "lucide-react";
 
-const SendInput = ({setAttachMenuOpen, menuBtnRef, setEmojisOpen, emojiBtnRef, message, setMessage }) => {
-    const dispatch = useDispatch();
+const SendInput = ({
+  setAttachMenuOpen,
+  menuBtnRef,
+  setEmojisOpen,
+  emojiBtnRef,
+  message,
+  setMessage,
+}) => {
+  const dispatch = useDispatch();
 
-    const { selectedUser } = useSelector(store => store.user);
+  const { selectedUser } = useSelector((store) => store.user);
 
-    const { messages } = useSelector(store => store.message);
+  const { messages } = useSelector((store) => store.message);
 
-    const onSubmitHandler = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/v1/message/send-text/${selectedUser?._id}`,
-                { message },
+  // open audio recording handler
 
-                {
-                    headers: {
-                        'Content-Type' : 'application/json'
-                    },
+  const openAudioRecord = () => {
+        console.log("recording...")
+  }
 
-                    withCredentials: true
-                });
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/v1/message/send-text/${
+          selectedUser?._id
+        }`,
+        { message },
 
-            dispatch(setMessages([...messages, res?.data?.newMessage]))
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
 
-        } catch (error) {
-            toast.error(error.response.data.message);
-            console.error(error);
+          withCredentials: true,
         }
+      );
 
-       setMessage("");
+      dispatch(setMessages([...messages, res?.data?.newMessage]));
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.error(error);
     }
 
+    setMessage("");
+  };
 
-    return (
-        <form
-            onSubmit={onSubmitHandler}
-            className="fixed bottom-0 right-0 md:w-[calc(100%-448px)] w-full px-4 md:px-12 py-3 bg-white shadow-[0_-2px_8px_rgba(0,0,0,0.1)] z-10"
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="fixed bottom-0 right-0 md:w-[calc(100%-448px)] w-full px-4 md:px-12 py-3 bg-white shadow-[0_-2px_8px_rgba(0,0,0,0.1)] z-10"
+    >
+      <div className="relative w-full max-w-screen-lg mx-auto">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          placeholder="Type a message..."
+          className="w-full text-gray-600 border border-gray-300 bg-white focus:outline-none focus:ring-1 text-sm rounded-lg pl-18 pr-10 py-3"
+        />
+        <div className="absolute left-3 top-3 inset-y-0 text-blue-500">
+          <div className="flex gap-1">
+            <Plus
+              ref={menuBtnRef}
+              size={24}
+              className="cursor-pointer"
+              onClick={() => setAttachMenuOpen((p) => !p)}
+            />
+            <SmilePlus
+              ref={emojiBtnRef}
+              size={23}
+              className="cursor-pointer"
+              onClick={() => setEmojisOpen((p) => !p)}
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className={`${
+            message ? `text-blue-500` : `text-gray-600`
+          } absolute inset-y-0 right-3 flex items-center cursor-pointer`}
         >
-            <div className="relative w-full max-w-screen-lg mx-auto">
-                <input
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    type="text"
-                    placeholder="Type a message..."
-                    className="w-full text-gray-600 border border-gray-300 bg-white focus:outline-none focus:ring-1 text-sm rounded-lg pl-18 pr-10 py-3"
-                />
-                <div className='absolute left-3 top-3 inset-y-0 text-blue-500'>
-                    <div className='flex gap-1'>
-                       <Plus ref={menuBtnRef} size={24} className='cursor-pointer' onClick={() => setAttachMenuOpen(p=>!p)}/>
-                       <SmilePlus ref={emojiBtnRef} size={23} className='cursor-pointer' onClick={() => setEmojisOpen(p=>!p)} />
-                     </div>       
-                </div>
-                <button 
-                type="submit" 
-                className={`${message ? `text-blue-500` : `text-gray-600`} absolute inset-y-0 right-3 flex items-center cursor-pointer`}>
-                    <IoSend
-                     size={21}   
-                    />
-                </button>
-            </div>
-     </form>
-    )
-}
+          {message ? <IoSend size={21} /> : <Mic size={23} className=" hover:text-blue-500" onClick={openAudioRecord} />}
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default SendInput;
